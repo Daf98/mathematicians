bf0f3b612d6d135f359d37ed1ba3bc1dafa0c3ca
Daf <dafne.azzolina@gmail.com>
2022-06-29 19:24:41 -0300

    Add basic functionality to calculator

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Input extends React.PureComponent {
   render() {
+    const { props } = this.props;
     return (
-      <div id="input">0</div>
+      <div id="input">{props}</div>
 
     );
   }
 }
+
+Input.propTypes = {
+  props: PropTypes.number.isRequired,
+};
 export default Input;

628e2abcce90acea83ba0b7370a4e7df1120b9a0
Daf <dafne.azzolina@gmail.com>
2022-06-29 21:20:58 -0300

    experiment II

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 
 class Button extends React.PureComponent {
   render() {
-    const { id, name, changeState } = this.props;
+    const { buttonName, changeState, className } = this.props;
     return (
-      <button type="button" className="single-button" id={id} onClick={() => { changeState(name); }}><h3>{name}</h3></button>
+      <button type="button" onClick={() => { changeState(buttonName); }}><h3>{buttonName}</h3></button>
     );
   }
 }
 
 Button.propTypes = {
-  name: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
+  buttonName: PropTypes.string.isRequired,
   changeState: PropTypes.func.isRequired,
+  className: PropTypes.string.isRequired,
 };
 export default Button;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,6 +3,85 @@ import Button from './Button';
 import Input from './Input';
 import calculate from '../logic/calculate';
 
+const butons = [{
+  buttonName: 'AC',
+  className: 'gray'
+},
+{
+  buttonName: '+/-',
+  className: 'gray'
+},
+{
+  buttonName: '%',
+  className: 'gray'
+},
+{
+  buttonName: '÷',
+  className: 'orange'
+},
+{
+  buttonName: '7',
+  className: 'gray'
+},
+{
+  buttonName: '8',
+  className: 'gray'
+},
+{
+  buttonName: '9',
+  className: 'gray'
+},
+{
+  buttonName: 'x',
+  className: 'orange'
+},
+{
+  buttonName: '4',
+  className: 'gray'
+},
+{
+  buttonName: '5',
+  className: 'gray'
+},
+{
+  buttonName: '6',
+  className: 'gray'
+},
+{
+  buttonName: '-',
+  className: 'orange'
+},
+{
+  buttonName: '1',
+  className: 'gray'
+},
+{
+  buttonName: '2',
+  className: 'gray'
+},
+{
+  buttonName: '3',
+  className: 'gray'
+},
+{
+  buttonName: '+',
+  className: 'orange'
+},
+{
+  buttonName: '0',
+  className: 'gray'
+},
+{
+  buttonName: '.',
+  className: 'gray'
+},
+{
+  buttonName: '=',
+  className: 'gray'
+}
+];
+
+const uniqueBatch = ['0', '.', '='];
 class Calculator extends React.PureComponent {
   constructor(props) {
     super(props);
@@ -28,27 +107,13 @@ class Calculator extends React.PureComponent {
         <Input props={next || total || '0'} />
         <div className="calculator-container">
           <div className="normal">
-            <Button name="AC" id="gray" changeState={this.changeState} />
-            <Button name="+/-" id="gray" changeState={this.changeState} />
-            <Button name="%" id="gray" changeState={this.changeState} />
-            <Button name="÷" id="orange" changeState={this.changeState} />
-            <Button name="7" id="gray" changeState={this.changeState} />
-            <Button name="8" id="gray" changeState={this.changeState} />
-            <Button name="9" id="gray" changeState={this.changeState} />
-            <Button name="x" id="orange" changeState={this.changeState} />
-            <Button name="4" id="gray" changeState={this.changeState} />
-            <Button name="5" id="gray" changeState={this.changeState} />
-            <Button name="6" id="gray" changeState={this.changeState} />
-            <Button name="-" id="orange" changeState={this.changeState} />
-            <Button name="1" id="gray" changeState={this.changeState} />
-            <Button name="2" id="gray" changeState={this.changeState} />
-            <Button name="3" id="gray" changeState={this.changeState} />
-            <Button name="+" id="orange" changeState={this.changeState} />
+            {firstBatch.map((fuckyou, index, array) => <Button key={fuckyou} buttonName={fuckyou} className={`${index === array.length - 1 ? 'orange' : 'gray'}`} changeState={this.changeState} />)}
+            {secondBatch.map((fuckyou, index, array) => <Button key={fuckyou} buttonName={fuckyou} className={`${index === array.length - 1 ? 'orange' : 'gray'}`} changeState={this.changeState} />)}
+            {thirdBatch.map((fuckyou, index, array) => <Button key={fuckyou} buttonName={fuckyou} className={`${index === array.length - 1 ? 'orange' : 'gray'}`} changeState={this.changeState} />)}
+            {fourthBatch.map((fuckyou, index, array) => <Button key={fuckyou} buttonName={fuckyou} className={`${index === array.length - 1 ? 'orange' : 'gray'}`} changeState={this.changeState} />)}
           </div>
           <div className="unique">
-            <Button name="0" id="gray" changeState={this.changeState} />
-            <Button name="." id="gray" changeState={this.changeState} />
-            <Button name="=" id="orange" changeState={this.changeState} />
+            {uniqueBatch.map((fuckyou) => <Button key={fuckyou} buttonName={fuckyou} className="gray" changeState={this.changeState} />)}
           </div>
 
         </div>
